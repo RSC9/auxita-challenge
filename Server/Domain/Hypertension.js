407fff5a12222671ce19c17f2d5eeb4475b6e56f
Raúl Santa Cruz <raul.santacruz@improving.com>
2021-07-13 15:42:12 -0500

    End-point to GET Hypertension readings

---
+++ Server/Domain/Hypertension.js
@@ -0,0 +1,6 @@
+let hypertensionReadings = [
+    { SysBP: 120, DiaBP: 90, atDate: '2018/10/31' },
+    { SysBP: 115, DiaBP: 100, atDate: '2018/10/20' }
+];
+
+module.exports = hypertensionReadings;
--- Server/index.js
+++ Server/index.js
@@ -5,12 +5,18 @@ const port = 3080;
 const Game = require('./Domain/Game');
 const game = new Game(50, 20);
 
+let hypertension = require('./Domain/Hypertension');
+
 let universe = [];
 
 app.use(bodyParser.json());
 
 app.get('/api/universe', (req, res) => {
     res.json(universe);
+});
+
+app.get('/api/hypertension', (req, res) => {
+    res.json(hypertension)
 });
 
 app.get('/', (req,res) => {
