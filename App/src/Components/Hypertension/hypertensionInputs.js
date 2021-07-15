0c664c820c856275441a234655469380dff1f828
Raúl Santa Cruz <raul.santacruz@improving.com>
2021-07-15 11:38:14 -0500

    Adding moment-date library to manage Datepicker

--- App/src/Components/Hypertension/hypertensionInputs.js
+++ App/src/Components/Hypertension/hypertensionInputs.js
@@ -1,26 +1,28 @@
 import React,  { useState } from 'react';
 import { Form, Input, InputNumber, DatePicker, Button } from 'antd';
+import moment from 'moment';
+import { defaultDateFormat } from '../../Shared/Constants/dateFormats';
 
 const HypertensionInputs = (props) => {
     const { addHypertensionReading } = props;
 
     const [SysBP, setSysBP] = useState('');
     const [DiaBP, setDiaBP] = useState('');
-    // const [atDate, setAtDate] = useState('21/09/1994')
+    const [atDate, setAtDate] = useState(moment());
 
     return (
         <Form
             layout="inline"
-            onFinish={() => addHypertensionReading({ SysBP, DiaBP })}
+            onFinish={() => addHypertensionReading({ SysBP, DiaBP, atDate })}
         >
             <Form.Item label="SysBP">
                 <InputNumber value={SysBP} onChange={(numberValue) => setSysBP(numberValue)} />
             </Form.Item>
             <Form.Item label="DiaBP">
-                <InputNumber value={DiaBP} />
+                <InputNumber value={DiaBP} onChange={(numberValue) => setDiaBP(numberValue)} />
             </Form.Item>
             <Form.Item label="Date">
-                <DatePicker />
+                <DatePicker defaultValue={atDate} format={defaultDateFormat} onChange={(momentDate) => setAtDate(momentDate)} />
             </Form.Item>
             <Form.Item>
                 <Button type="primary" htmlType="submit">
