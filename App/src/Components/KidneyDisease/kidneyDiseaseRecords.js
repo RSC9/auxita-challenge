ba8c9b8a3a3f1d8b946d8b7f6eb6bacfb65ebe2c
Raúl Santa Cruz <raul.santacruz@improving.com>
2021-07-21 00:26:25 -0500

    + getTwoConsecutiveItems()

--- App/src/Components/KidneyDisease/kidneyDiseaseRecords.js
+++ App/src/Components/KidneyDisease/kidneyDiseaseRecords.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Table } from 'antd';
 import kidneyDiseaseColumns from '../../Shared/Constants/KidneyDisease/kidneyDiseaseColumns';
+import { getTwoConsecutiveItems } from '../../Shared/Helpers/arrays';
 
 const KidneyDiseaseRecords = (props) => {
     const { kidneyDiseaseReadings } = props;
 
+    const expandedRowRender = (reading) => {
+        const currentItem = kidneyDiseaseReadings.indexOf(reading);
+        const data = getTwoConsecutiveItems(currentItem, kidneyDiseaseReadings);
+
+        return (
+            <Table rowKey={record => record.id} columns={kidneyDiseaseColumns} dataSource={data} pagination={false} size="small" />
+        );
+    };
+
     return (
-        <Table rowKey={record => record.id} dataSource={kidneyDiseaseReadings} columns={kidneyDiseaseColumns} />
+        <Table
+            rowKey={record => record.id}
+            dataSource={kidneyDiseaseReadings}
+            columns={kidneyDiseaseColumns}
+            expandable={{ expandedRowRender }}
+        />
     );
 };
 
--- App/src/Shared/Helpers/arrays.js
+++ App/src/Shared/Helpers/arrays.js
@@ -1,3 +1,7 @@
 export const sortObjectArrayByDate = (array) => {
     return array.sort((a, b) => new Date(b.atDate) - new Date(a.atDate));
 };
+
+export const getTwoConsecutiveItems = (currentItem, array) => {
+    return array.filter((value, index) => index === currentItem + 1 || index === currentItem + 2);
+};
