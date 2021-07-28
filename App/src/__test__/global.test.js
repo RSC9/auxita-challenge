1fae4b1c00e2319dc7385e7034a87b3a16f2f85a
Raúl Santa Cruz <raul.santacruz@improving.com>
2021-07-28 18:53:39 -0500

    Implementing tests to promises
    + afterEach()
    + afterAll()
    
    + beforeEach()
    + beforeAll()

--- App/src/__test__/global.test.js
+++ App/src/__test__/global.test.js
@@ -26,3 +26,30 @@ test('Probar un callback', () => {
         expect(str).toBe('aloH');
     });
 });
+
+const reverseString2 = str => {
+    return new Promise((resolve, reject) => {
+        if (!str) {
+            reject(Error('Error'));
+        }
+        resolve(str.split("").reverse().join(""));
+    });
+};
+
+test('Probar una promesa', () => {
+    return reverseString2('Hola')
+        .then(string => {
+            expect(string).toBe('aloH')
+        });
+});
+
+test('Probar async/await', async () => {
+    const string = await reverseString2('hola');
+    expect(string).toBe('aloh');
+});
+
+afterEach(() => console.log('Después de cada prueba'));
+afterAll(() => console.log('Después de todas las pruebas'));
+
+beforeEach(() => console.log('Antes de cada prueba'));
+beforeAll(() => console.log('Antes de todas las pruebas'));
