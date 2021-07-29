f124ba1afa18ed0ffc5c531dffacaef03be318bc
Raúl Santa Cruz <raul.santacruz@improving.com>
2021-07-29 15:36:08 -0500

    Watch and Coverage
    + "test:watch": "jest --watch"
    + jest --coverage

---
+++ App/src/__test__/vesflip.test.js
@@ -0,0 +1,16 @@
+const randomString = require('../vesflip');
+
+// test('Probar la funcionalidad', () => {
+//     expect(typeof (randomString())).toBe('string');
+// });
+
+describe('Probar funcionalidades de randomStrings', () => {
+
+    test('Probar la funcionalidad', () => {
+        expect(typeof (randomString())).toBe('string');
+    });
+    
+    test('Comprobar que no existe una ciudad', () => {
+        expect(randomString()).not.toMatch(/Cordoba/);
+    });
+});
