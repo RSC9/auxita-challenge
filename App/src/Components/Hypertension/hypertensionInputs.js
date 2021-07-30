1cf9584b0b39a107e97919b88e61e89ea994e6f3
Raúl Santa Cruz <raul.santacruz@improving.com>
2021-07-30 12:02:44 -0500

    MOCKS for Jest with React and AntDesign

--- App/src/Components/Hypertension/hypertensionInputs.js
+++ App/src/Components/Hypertension/hypertensionInputs.js
@@ -21,7 +21,7 @@ const HypertensionInputs = (props) => {
                 <InputNumber onChange={(numberValue) => setDiaBP(numberValue)} />
             </Form.Item>
             <Form.Item label="Date" name="atDate" rules={[{ required: true, message: 'Please input a Date.' }]}>
-                <DatePicker format={defaultDateFormat} onChange={(momentDate, dateString) => setAtDate(dateString)} />
+                {/* <DatePicker format={defaultDateFormat} onChange={(momentDate, dateString) => setAtDate(dateString)} /> */}
             </Form.Item>
             <Form.Item>
                 <Button type="primary" htmlType="submit">
--- App/src/__test__/components/hypertension/hypertensionInputs.test.js
+++ App/src/__test__/components/hypertension/hypertensionInputs.test.js
@@ -1,11 +1,13 @@
+import 'jsdom-global/register'; //at the top of file , even  , before importing react
+import '../../../__mocks__/matchMedia.mock';
 import React from 'react';
 import { mount } from 'enzyme';
 import HypertensionInputs from '../../../Components/Hypertension/hypertensionInputs';
 
 describe('<HypertensionInputs />', () => {
+    const hypertensionInputs = mount(<HypertensionInputs />);
 
     test('Render of HypertensionInputs component', () => {
-        const hypertensionInputs = mount(<HypertensionInputs />);
 
         expect(hypertensionInputs.length).toEqual(1);
     });
